35b1d3899b48765e5f166bd2dedab9457a5fbace
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-14 20:39:40 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  let my_table = document.getElementById("xxx").innerHTML;
+	let my_element = `<tr><td>New Cell1</td> 
+		<td>New Cell2</td></tr>`;
+	my_table = my_element+my_table;
+	// console.log(my_table);
+	return false;
 }
